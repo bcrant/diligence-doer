8917baea53a9cc4207d47f086e9e91cc3a7c8f94
Brian Crant <bcrant@gmail.com>
2021-08-23 15:54:13 -0700

    Parse all columns and table names from pipeline SQL.

--- parse_pipelines.py
+++ parse_pipelines.py
@@ -1,30 +1,79 @@
 #!/usr/bin/env python
 
-import json
 import yaml
 import pprint
-from mo_sql_parsing import parse
+import sqlparse
+import sql_metadata
+from sql_metadata import Parser, compat
 
 
 def table_name_fmt(name):
     return name.replace('-', '_')
 
 
-deps_dict = {}
 commands_dict = {}
 
 with open("example.yml", 'r') as stream:
     steps_dict = yaml.safe_load(stream)['steps']
 
     for step in steps_dict:
-        if step.get('step_type') == 'sql-command':
-            deps_dict[table_name_fmt(step.get('name'))] = step.get('depends_on')
-            commands_dict[table_name_fmt(step.get('name'))] = step.get('command')
+        # if step.get('step_type') == 'sql-command':
+        # isolate one table for testing only
+        if step.get('name') == 'sponsored-learning-enrollment-activities':
+            # FORMAT
+            formatted_statement = sqlparse.format(
+                step.get('command'),
+                reindent=True,
+                strip_comments=True,
+                keyword_case='upper',
+                comma_first=True
+            )
 
-            parse(step.get('command'))
+            # SPLIT
+            split_statement = sqlparse.split(formatted_statement)
 
-            # try this one instead
-            # https://github.com/andialbrecht/sqlparse
+            # STORE
+            commands_dict[table_name_fmt(step.get('name'))] = split_statement
 
-# pprint.pprint(deps_dict)
-# pprint.pprint(commands_dict)
+query = commands_dict.get('sponsored_learning_enrollment_activities')
+for q in query:
+    if str(sqlparse.parse(q)[0].token_first()) == 'INSERT':
+        # print(type(q), q)
+        tables = [t for t in Parser(q).tables]
+        pprint.pprint(tables)
+
+        columns = [c for c in Parser(q).columns]
+        pprint.pprint(columns)
+
+        # columns_dict = [cd for cd in Parser(q).columns]
+        # pprint.pprint(columns_dict)
+
+        alias_dict = {}
+        for col in Parser(q).columns:
+            if Parser(q).columns_aliases_dict is not None:
+
+
+
+        print('\n\n')
+
+
+    # try:
+    #     sql_metadata.compat.get_query_tables(q)
+    # except e:
+    #     print(e)
+
+    # # print(type(q), len(q), q)
+    # parsed = sqlparse.parse(q)[0]
+    # tokens = parsed.get_sublists()
+    # print('q: ', q)
+    # print('tokens: ', parsed.tokens)
+    # print('# of tokens: ', len(parsed.tokens))
+    # print('get_type: ', parsed.get_type())
+    # print('get_sublists: ', next(parsed.get_sublists()))
+    # print('\n\n')
+
+# for phrase in query:
+#     print(type(phrase), phrase)
+#     # print(phrase.split('CREATE TABLE'))
+#     # x = Parser(phrase).columns
+#     # print(x)
